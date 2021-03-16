ffeee5e61e6df65c5ce34bc4e440027efa1e3c76
Kirill0798 <kmusin@mail.ru>
2021-03-17 01:10:26 +0400

    added logger

--- client.py
+++ client.py
@@ -1,14 +1,30 @@
 import socket
+import ssl
+import logging
+
+IMAGE_PATH = "image/foto.jpg"
+MAX_FILE_SIZE = 2048
+HOST, PORT = 'localhost', 10002
+LOG_PATH = "logs/client.log"
+FORMAT = "%(asctime)s - %(message)s"
+
+logging.basicConfig(filename=LOG_PATH, level=logging.DEBUG, format=FORMAT)
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # AF_INET = IP, SOCK_STREAM = TCP
-client.connect(('localhost', 10002))  # 127.0.0.1
+client.connect((HOST, PORT))  # 127.0.0.1
+logging.info("Клиент подключился к серверу")
 
-file = open('image/foto.jpg', 'rb')
-image_data = file.read(2048)
+file = open(IMAGE_PATH, 'rb')
+logging.info("Открыт файл:{img_path}".format(img_path=IMAGE_PATH))
+
+image_data = file.read(MAX_FILE_SIZE)
 
 while image_data:
     client.send(image_data)
-    image_data = file.read(2048)
+    image_data = file.read(MAX_FILE_SIZE)
+
+logging.info("Изображение отправлено")
 
 file.close()
 client.close()
+logging.info("Завершение работы")
--- server.py
+++ server.py
@@ -2,34 +2,49 @@ import socket
 import cv2
 import logging
 
-logging.basicConfig(filename="logs/server.log", level=logging.DEBUG, format="%(asctime)s - %(message)s")
+LOG_PATH = "logs/server.log"
+FORMAT = "%(asctime)s - %(message)s"
+HOST, PORT = 'localhost', 1235
+RECEIVED_FILE_PATH = "image/server_image.jpg"
+MANAGED_FILE_PATH = "image/sp_without_noise.jpg"
+MAX_FILE_SIZE = 2048
+MAX_RANG = 3
+
+logging.basicConfig(filename=LOG_PATH, level=logging.DEBUG, format=FORMAT)
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # AF_INET = IP, SOCK_STREAM = TCP
-server.bind(('localhost', 1235))  # 127.0.0.1
+server.bind((HOST, PORT))  # 127.0.0.1
 server.listen()
 logging.info("Ожидание подключения...")
 
 while True:
     client_socket, client_address = server.accept()
+    logging.info("Принято подключение клиента с url: {address}:{c_socket}"
+                 .format(address=client_address, c_socket=client_socket))
 
-    file = open('image/server_image.jpg', "wb")
-    image_chunk = client_socket.recv(2048)  # stream-based protocol
+    file = open(RECEIVED_FILE_PATH, "wb")
+    logging.info("Открыт файл для записи: {file_path}".format(file_path=RECEIVED_FILE_PATH))
+
+    image_chunk = client_socket.recv(MAX_FILE_SIZE)  # stream-based protocol
+    logging.info("Получен файл от клиента")
 
     while image_chunk:
         file.write(image_chunk)
-        image_chunk = client_socket.recv(2048)
+        image_chunk = client_socket.recv(MAX_FILE_SIZE)
         if not image_chunk:
             break
 
     file.close()
+    logging.info("Необработанный файл записан")
 
+    image = cv2.imread(RECEIVED_FILE_PATH)
+
+    logging.info("Идет восстановление изображения")
+    image = cv2.medianBlur(image, MAX_RANG)
+
+    cv2.imwrite(MANAGED_FILE_PATH, image)
+    logging.info("Изображение восстановлено")
 
 client_socket.close()
 
-image = cv2.imread('image/server_image.jpg')
 
-logging.info("Идет восстановление изображения")
-image = cv2.bilateralFilter(image, 5, 20, 20)
-
-cv2.imwrite('image/sp_without_noise.jpg', image)
-logging.info("Изображение восстановлено")
